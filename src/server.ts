d5a912afb9a5d63c6e1c8be9e3902e92806c1558
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-02 11:09:24 -0500

    add CORS permition to url frontend

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes'
 
@@ -9,6 +11,9 @@ dotenv.config()
 connectDB()
 
 export const app =  express();
+
+// permitir las conexiones
+app.use(cors(corsConfig))
 
 // leer datos formato json
 app.use( express.json())
