3d16cf9a437fe9cdeb6ebf17fa0ec2d5f6137f43
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-10 09:48:28 -0500

    create route auth

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import cors from 'cors'
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes'
+import authRoutes from './routes/authRoutes';
+
 
 // toma las varaibles de entorno
 dotenv.config()
@@ -20,3 +22,4 @@ app.use( express.json())
 
 // haciendole saber las rutas al server
 app.use('/api/projects' , projectRoutes )
+app.use('/api/auth' , authRoutes )
