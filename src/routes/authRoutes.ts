8ba5bc0e6acf5c18541d32999970edb2d170eafe
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-10 11:04:03 -0500

    add controller and validatio to create account enpoint

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,32 @@
 import { Router } from 'express'
+import { AuthController } from '../controllers/AuthController'
+import { body } from 'express-validator'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.get('/' , ( req ,res ) => { 
-    res.send('desde /api/auth')
-})
+router.post('/create-account' , 
+    
+    body('name')
+        .notEmpty().withMessage('El Nombre no puede ir Vacio'),
+
+    body('email')
+        .isEmail().withMessage('El Email no puede ir Vacio'),
+
+    body('password')
+        .isLength({min : 8}).withMessage('El password es muy corto , debe ser mayor a 8  caracteres'),
+
+    body('password_confirmation').custom(( value , { req }) => {
+
+        if( value !== req.body.password) { 
+            throw new Error('Las Password no son iguales')
+        }
+
+        return true
+    }),
+
+    handleInputErrors,
+    
+AuthController.createAccount )
 
 export default router
