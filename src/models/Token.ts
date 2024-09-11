e73929bd8095c872de73482c954a4ab91dae7ec7
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-11 08:38:03 -0500

    token model

---
+++ src/models/Token.ts
@@ -0,0 +1,26 @@
+import mongoose , { Schema , Document, Types} from "mongoose";
+
+export interface Itoken extends Document { 
+    token : string
+    user : Types.ObjectId // informacion del usuario
+    createdAt : Date
+}
+
+const tokenSchema : Schema = new Schema ({ 
+    token : { 
+        type : String,
+        required : true
+    },
+    user : { 
+        type : Types.ObjectId,
+        ref : 'User' // donde encontrara la informacion
+    },
+    createdAt : { 
+        type : String,
+        default :  Date.now(), // genera le fecha en automatico una vez se crea el objeto
+        expires : '10m' // despues de un 1 se elimina en automatico
+    },
+})
+
+const Token = mongoose.model<Itoken>('Token' , tokenSchema )
+export default Token
