22dd846d0ebed040738fc77a7d2221cfda1aa54b
Franklin-Gomez <franklin_043097@hotmail.com>
2024-09-19 09:23:55 -0500

    new endpoint user

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -249,4 +249,8 @@ export class AuthController {
 
         }
     }
+
+    static user = async ( req : Request , res : Response ) => { 
+        return res.json( req.user )
+    }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthController } from '../controllers/AuthController'
 import { body, param } from 'express-validator'
 import { handleInputErrors } from '../middleware/validation'
+import { authenticate } from '../middleware/auth'
 
 const router = Router()
 
@@ -97,5 +98,11 @@ router.post('/update-password/:token' ,
     handleInputErrors,
 
 AuthController.UpdatePasswordWithToken)
+
+router.get('/user' , 
+
+    authenticate,
+
+AuthController.user)
 
 export default router
